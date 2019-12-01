1e279ab1474947282b2c3ef657471919b5247e04
Eoan O'Dea <odeaeoan@gmail.com>
2019-12-01 12:37:13 +0100

    Fixed VisitsTableSeeder and commented code

--- database/seeds/VisitsTableSeeder.php
+++ database/seeds/VisitsTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
 use App\Role;
 use App\Visit;
 
@@ -14,26 +13,27 @@ class VisitsTableSeeder extends Seeder
      */
     public function run()
     {
+        //Define faker for filling visit with fake data
         $faker = \Faker\Factory::create();
 
+        // Get all users with the role of patient and doctor
         $role_patient = Role::where('name', 'patient')->first();
         $role_doctor = Role::where('name', 'doctor')->first();
+        $doctors = $role_doctor->users;
 
+        // Loop through patients and create a visit for each
         foreach($role_patient->users as $user) {
-            $doctors = $role_doctor->users;
+            //Get a random doctor and assign them to the visit
             $doctor = $doctors[rand(0, count($doctors) -1)];
-            echo $doctor;
             
-            // echo '\n single doctor';
-            // echo $doctor;
             $visit = new Visit();
-
+            //Populate visit with faker data
             $visit->notes = $faker->paragraph();
             $visit->duration = $faker->randomFloat($nbMaxDecimals = NULL, $min = 5, $max = 180);
-
+            //Assign foreign keys to visit
             $visit->patient_id = $user->patient->id;
             $visit->doctor_id = $doctor->doctor->id;
-
+            //save visit
             $visit->save();
         }    
     }
